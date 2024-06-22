6edec8f8f9f67b3a3d327e46bfa30c1363317f31
charleysmithschofield <charleysmithschofield@gmail.com>
2024-06-22 16:40:51 -0700

    add benchmark method to return elapsed time in seconds

--- benchmark_with_block.rb
+++ benchmark_with_block.rb
@@ -1,5 +1,12 @@
+# benchmark_with_block.rb
+
+# Method to benchmark the execution time of a block
 def benchmark
   # Your benchmarking code goes here.
+  start_time = Time.now
+  yield
+end_time = Time.now
+end_time - start_time # Return the elapsed time in seconds
 end
  
 # Be careful, pasting this into IRB will take a long time to print.
